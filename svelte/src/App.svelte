1f037ba3e5eb8bff65b0a8953807e128df15b9b4
John Paul Jaquith <japesjake@gmail.com>
2022-10-17 22:41:30 -0400

    added makeTables function to Tables.svelte for access to reactive local storage.

--- svelte/src/App.svelte
+++ svelte/src/App.svelte
@@ -1,17 +1,8 @@
 <script>
-	import { min, max, people, tables } from './stores.js'
-	import { makeTables } from './scripts/makeTables.js'
-	import { assignCouples } from './scripts/assignCouples.js'
+	// import { min, max, people, tables } from './stores.js'
 	import People from './components/People.svelte'
 	import Max from './components/Max.svelte'
 	import Tables from './components/Tables.svelte'
-	$: $min, $max, $people, $tables, update()
-	// $: console.log($people)
-
-function update() {
-	makeTables()
-	assignCouples()
-}
 
 </script>
 
--- svelte/src/components/Tables.svelte
+++ svelte/src/components/Tables.svelte
@@ -1,7 +1,65 @@
 <script>
-    import { set_attributes } from 'svelte/internal';
-import { people, tables } from '../stores.js'
+import { people, max, min, tables } from '../stores.js'
+
+function sum (array) {
+    let total = 0
+    for (let i = 0; i < array.length; i++){
+        total += array[i]
+    }
+    return total
+}
+
+function getTotalGuests() {
+    let total = 0;
+    for (let i = 0; i < $people.length; i++) {
+        if ($people[i].hasSO) {
+            total += 2
+        }else{
+            total += 1
+        }
+    }
+    return total
+}
+
+function makeTables () {
+    let totalGuests = getTotalGuests()
+    if ($max <= 0) {return}
+    let numTables = Math.ceil(totalGuests / $max)
+    let nums = []
+    for (let i = 0; i < numTables; i++) {
+        nums.push($max)
+    }
+    let totalSubtractor = 0;
+    let subtractor = 0;
+    while (sum(nums) != totalGuests) {
+        subtractor = totalSubtractor;
+        for (let i = 0; i < numTables; i++) {
+            if (subtractor) {
+                nums[i] -= 1
+                subtractor -= 1
+            }
+            if (sum(nums) == totalGuests) {
+                break
+            }else{
+                totalSubtractor++
+            }
+        }  
+    }
+    tables.set(nums)
+}
+
+function assignCouples () {
+    
+}
+
+function update () {
+    makeTables()
+    assignCouples()
+}
+
+$: $max, $min, $people, update()
 </script>
+
 <main>
     {#each $tables as table, id}
     Table {id + 1} has {table} seats.<br>
