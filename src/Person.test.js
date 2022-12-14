91ec54b1a08a6b0ff17c21a09ca762025a67bd67
japesjake <japesjake@gmail.com>
2022-12-14 02:54:34 -0500

    added tests for isoupledWith() and isCoupled().

--- src/Person.test.js
+++ src/Person.test.js
@@ -5,4 +5,16 @@ describe('Person', () => {
 		const person = new Person('John', 'Doe')
 		expect(person.fullName).toBe('John Doe')
 	})
+	it('should return true if man and woman are coupled', () => {
+        const man = new Person('John', 'Doe') 
+		const woman = new Person('Jane', 'Doe')
+        man.coupleWith(woman)
+        expect(man.isCoupled() && woman.isCoupled()).toBe(true)
+	})
+	it('should return true if man is coupled with woman', () => {
+        const man = new Person('John', 'Doe') 
+		const woman = new Person('Jane', 'Doe')
+        man.coupleWith(woman)
+        expect(man.isCoupledWith(woman) && woman.isCoupledWith(man)).toBe(true)
+	})
 })
