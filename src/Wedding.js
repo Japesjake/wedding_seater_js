970d22cb2d6797675c7f30f03ec967568b9e2368
japesjake <japesjake@gmail.com>
2023-01-04 01:51:45 -0500

    split assignment method into methods for assigning couples and singles.

--- src/Wedding.js
+++ src/Wedding.js
@@ -24,24 +24,36 @@ export default class Wedding {
         }
         return true
     }
+    assignCouple(person, table) {
+        for (let i = 0; i < this.people; i++) {
+            if (this.people[i].isCoupledWith(person)) {
+                let assigned = people[i].assignTo(table)
+                console.log(assigned)
+                let assigned2 = person.assignTo(table)
+                console.log(assigned2)
+                if (assigned && assigned2) {
+                    this.removePersonById(i)
+                    this.removePersonById(0)
+                }
+            }
+        }
+    }
+    assignPerson(person, table) {
+        let assigned = person.assignTo(table)
+        if (assigned) {
+            this.removePersonById(0)
+        }
+    }
     wildAssign() {
+        // console.log(this.people)
         while (this.people != [] && !this.areFull()) {
             for (let table of this.tables) {
                 let person = this.people[0]
+                // console.log('updated: ' + this.people)
                 if (person.isCoupled()) {
-                    for (let i = 0; i < this.people; i++) {
-                        if (spouse.isCoupledWith(person)){
-                            let assigned = spouse.assignTo(table)
-                            if (assigned) {
-                                this.removePersonById(i)
-                            }
-                        }
-
-                    }
-                }
-                let assigned = person.assignTo(table)
-                if (assigned) {
-                    this.removePersonById(0)
+                    this.assignCouple(person, table)
+                } else {
+                    this.assignPerson(person, table)
                 }
             }
         }
